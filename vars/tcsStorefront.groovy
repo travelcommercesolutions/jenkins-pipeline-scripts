054aeb059aa590b75355a97156724a691cd6e6ea
romansoftteco <roman@softteco.com>
2020-03-20 23:22:37 +0300

    Fix typo

--- vars/tcsStorefront.groovy
+++ vars/tcsStorefront.groovy
@@ -109,7 +109,7 @@ def call(body) {
 							Packaging.saveArtifact(this, 'tcs', 'storefront', '', artifacts[0].path) // config.sampleStore for projects w def store
 						}
 						if (env.BRANCH_NAME == 'master') {
-							Strinng releaseTag = "v" 
+							String releaseTag = "v" 
 							releaseTag << version
 							Packaging.publishRelease(this, releaseTag, notes) // publish artifacts to github releases
 						}
